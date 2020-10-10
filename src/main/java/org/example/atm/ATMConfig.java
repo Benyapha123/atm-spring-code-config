3966b370ce1f8d3145881914d4f806df87b3b145
Benyapha123 <61050850@kmitl.ac.th>
2020-10-10 14:53:38 +0700

    first commit

---
+++ src/main/java/org/example/atm/ATMConfig.java
@@ -0,0 +1,23 @@
+package org.example.atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceDB();
+    }
+    @Bean
+    public Bank bank(){
+        return new Bank("My Bank",dataSource());
+    }
+    @Bean
+    public ATM atm(){
+        return new ATM(bank());
+    }
+    }
+
